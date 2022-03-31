0fc7d8b7a4c3c135f06721f17d24238056044811
1asset <82859085+1asset@users.noreply.github.com>
2022-03-31 12:50:03 +0600

    Update task12.py

--- tasks/task12.py
+++ tasks/task12.py
@@ -1,10 +1,59 @@
-n = int(input())
 
-print ("____________________________________________")
+from itertools import permutations
 
-a0 = 1
 
-for i in range(1, n+1):
-    a1 = (a0+1)/i
-    print(a1)
-    a0 = a1
+
+n =  int(input())
+
+
+if n % 100 == 0 :
+    print("-1")
+elif n%10==0:
+    print("-1")
+
+check = 0
+
+
+
+def checkdigit(N,n):
+
+    last_digit = N % 10 
+    digit2  = N // 10
+
+    middle_digit= n//10%10
+    first_digit = n//100
+
+    if ((first_digit != last_digit) and (middle_digit!=first_digit) and (middle_digit!=last_digit)):
+        
+        while (N != 0) :
+ 
+        
+            current_digit = digit2 % 10;
+        
+        
+            N = N // 10;
+ 
+    
+            if (current_digit == last_digit) :
+                return "No";
+        return "Yes"
+    
+        
+ 
+    
+    return "No";
+
+
+
+for i in range(2,100):
+    check = n * i 
+    if checkdigit(check,check)=="Yes":
+        print(check)
+        check == 0
+        break;
+    else:
+        check==0
+
+
+
+
