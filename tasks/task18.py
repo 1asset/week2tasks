59a264578c62710f14326d7df6791acbc7ca62dd
1asset <82859085+1asset@users.noreply.github.com>
2022-03-31 08:34:12 +0600

    Update task18.py

--- tasks/task18.py
+++ tasks/task18.py
@@ -1,1 +1,15 @@
+from math import sin
 
+n, a, b = int(input()), int(input()), int(input())
+
+h = round((b-a)/n)
+
+print(h)
+
+print ("______________________________")
+
+
+print(sin(a))
+
+for x in range (1, h):
+    print(1-sin(a+x*h))
