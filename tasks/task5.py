bd4436607a05e594c95e926a2e29210a1de852a7
1asset <82859085+1asset@users.noreply.github.com>
2022-03-31 08:31:06 +0600

    Update task5.py

--- tasks/task5.py
+++ tasks/task5.py
@@ -1,1 +1,14 @@
+from math import factorial
 
+x = int(input())
+n = int(input())
+
+total = 1
+
+for i in range(1, n+1):
+    if i%2 > 0:
+        total -= x/factorial(2*i)
+    elif i%2 == 0:
+        total += x/factorial(2*i)
+        
+print(total)
