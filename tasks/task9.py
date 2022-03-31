87bfa03da6e8217fd4eb5f33f4626616769fa528
1asset <82859085+1asset@users.noreply.github.com>
2022-03-31 12:49:31 +0600

    Update task9.py

--- tasks/task9.py
+++ tasks/task9.py
@@ -1,19 +1,49 @@
-from math import factorial
 
-x = float(input())
+from itertools import permutations
+
+
+
 n = int(input())
 
-total = 1+x/2
+array = []
 
-multiply = 2
-
-for q in range(2, n+1):
-    multiply *= q * 2
+for i in range(1,n+1):
     
-for i in range(1, n+1):
-    if i%2 > 0:
-        total += (2*i-3)*x/factorial(multiply)
-    elif i%2==0:
-        total -= (2*i-3)*x/factorial(multiply)
+    array.append(i)
+
+
+permutation = permutations(array)
+
+perm = list(permutation)
+
+arraycheck = []
+
+arraycheck1=[]
+
+plus = 0
+
+for i in perm:
+    arraycheck.append(i)
+
+    arraycheck1 = arraycheck[0]
+
+    arraycheck.clear()
+
+    for i in range(0,n-1):
+
+      plus +=  arraycheck1[n-1-i]-arraycheck1[n-1-(i+1)]
+      
+
+
+    
+    if (plus>0):
+
+        print("This permutaion in acending order")
+
+        print(arraycheck1)
+
         
-print(total)
+
+    else :
+
+        print(" ")
