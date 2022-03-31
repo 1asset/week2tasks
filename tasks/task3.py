f5e8384903083b25edef6b4b09233b1eec76390d
1asset <82859085+1asset@users.noreply.github.com>
2022-03-31 12:47:25 +0600

    Update task3.py

--- tasks/task3.py
+++ tasks/task3.py
@@ -1,14 +1,49 @@
-from math import factorial
+from itertools import permutations
 
-x = int(input())
+
+
 n = int(input())
 
-total = x
+array = []
 
-for i in range(1, n+1):
-    if i%2 > 0: 
-        total -= x/factorial(2*i+1)
-    elif i%2 == 0:
-        total += x/factorial(2*i+1)
+for i in range(1,n+1):
+    
+    array.append(i)
+
+
+permutation = permutations(array)
+
+perm = list(permutation)
+
+arraycheck = []
+
+arraycheck1=[]
+
+plus = 0
+
+for i in perm:
+    arraycheck.append(i)
+
+    arraycheck1 = arraycheck[0]
+
+    arraycheck.clear()
+
+    for i in range(0,n-1):
+
+      plus +=  arraycheck1[n-1-i]-arraycheck1[n-1-(i+1)]
+      
+
+
+    
+    if (plus>0):
+
+        print("This permutaion in acending order")
+
+        print(arraycheck1)
+
         
-print(total)
+
+    else :
+
+        print(" ")
+
