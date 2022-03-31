f74d0cc12bc28880e3a4712c8d92a9de6645fb90
1asset <82859085+1asset@users.noreply.github.com>
2022-03-31 08:33:53 +0600

    Update task17.py

--- tasks/task17.py
+++ tasks/task17.py
@@ -1,1 +1,18 @@
+n = int(input())
+print("_________________________________")
+if n == 1:
+    print('1')
+elif n == 2:
+    print('2')
+else:
+    print('1')
+    print('2')
+    
+a1 = 1
+a2 = 2
 
+for i in range(3, n+1):
+    a = (a1+2*a2)/i
+    print(a)
+    a1=a2
+    a2=a
